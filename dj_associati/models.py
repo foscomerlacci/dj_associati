3899e1aa320ea106b6040183af5e97df62fafe96
foscomerlacci <foscomerlacci@tiscali.it>
2018-11-05 09:15:36 +0100

    terzo commit

--- dj_associati/models.py
+++ dj_associati/models.py
@@ -5,12 +5,28 @@ from django.db import models
 from model_utils.models import TimeStampedModel
 
 
-class Socio(TimeStampedModel):
-    pass
-    
+class Socio(models.Model):
+    nome = models.CharField(max_length=30)
+    cognome = models.CharField(max_length=30)
+    discipline = models.ManyToManyField("Disciplina", blank=True)
 
-class Disciplina(TimeStampedModel):
-    pass
-    
+    class Meta:
+        verbose_name = 'socio'
+        verbose_name_plural = 'soci'
+
+    def __str__(self):
+        return self.nome + " " + self.cognome
+
+
+class Disciplina(models.Model):
+    disciplina = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = 'disciplina'
+        verbose_name_plural = 'discipline'
+
+    def __str__(self):
+        return self.disciplina
+
 
 
